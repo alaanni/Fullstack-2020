a4e8d2ed2c179ea82ae272c55e4d22804fc5675c
alaanni <anni.alamattinen@gmail.com>
2020-04-11 19:12:19 +0300

    uibuild

---
+++ osa2/puhelinluettelo/build/precache-manifest.3ce3c672ae875313c03579e729a80148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d64675ef246d5bd9d9392722037743",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab8c88bac1befadabeec",
+    "url": "/static/css/main.886b0048.chunk.css"
+  },
+  {
+    "revision": "f6915c6258628ffbe4b1",
+    "url": "/static/js/2.9a17e31d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9a17e31d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8c88bac1befadabeec",
+    "url": "/static/js/main.4e3e44ce.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
